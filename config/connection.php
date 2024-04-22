a2feae2b8ee90d7fd0e413a3116f7debbc96321b
maryyygb <marygbasilio8@gmail.com>
2024-04-22 15:42:12 +0800

    next

--- config/connection.php
+++ config/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST1', 'localhost');
-define('DB_USER1', 'rweb');
-define('DB_PASS1', '');
+define('DB_USER1', 'u416864070_data');
+define('DB_PASS1', 'M$f8bpWcm');
 define('DB_NAME1', 'u416864070_rweb');
 
 ?>
